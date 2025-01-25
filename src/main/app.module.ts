108da7fbc6c5f1bf0566f32531ed6c957379ec4c
Rodrigo César <rodrigocnn@gmail.com>
2025-01-25 20:08:46 -0300

    feat(module): add import UserModule to AppModule

--- src/main/app.module.ts
+++ src/main/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PropertyModule } from 'src/modules/property/property.module';
 import { CityModule } from 'src/modules/city/city.module';
+import { UserModule } from 'src/modules/user/user.module';
 
 @Module({
-  imports: [PropertyModule, CityModule],
+  imports: [PropertyModule, CityModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
