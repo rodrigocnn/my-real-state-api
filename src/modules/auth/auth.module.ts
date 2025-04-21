515c73c6560c8f79b1240cd6f03c3a645fa37d00
Rodrigo César <rodrigocnn@gmail.com>
2025-04-21 16:49:22 -0300

    chore(auth) add config jwt secret

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,14 +3,21 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule,
     UserModule,
-    JwtModule.register({
-      global: true,
-      secret: 'SECRET',
-      signOptions: { expiresIn: '72h' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: { expiresIn: '72h' },
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
