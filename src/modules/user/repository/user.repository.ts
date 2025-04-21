07588a85226b17028c0caef128883be9ee5a7a32
Rodrigo César <rodrigocnn@gmail.com>
2025-04-21 16:51:23 -0300

    feat(user): add  methods not implemented yet interface

--- src/modules/user/repository/user.repository.ts
+++ src/modules/user/repository/user.repository.ts
@@ -5,9 +5,8 @@ import { User } from '../model/user.model';
 export interface UserRepository {
   create(user: UserCreateDto): Promise<UserResponseDto | null>;
   findByEmail(email: string): Promise<User>;
-  // findAll(): Promise<UserResponseDto[]>;
-  // findById(id: string): Promise<UserResponseDto>;
-
-  // update(id: string, user: UserCreateDto): Promise<UserResponseDto | null>;
-  // delete(id: string): Promise<boolean>;
+  findAll(): Promise<UserResponseDto[]>;
+  findById(id: string): Promise<UserResponseDto>;
+  update(id: string, user: UserCreateDto): Promise<UserResponseDto | null>;
+  delete(id: string): Promise<boolean>;
 }
