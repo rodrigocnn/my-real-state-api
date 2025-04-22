0ede0aceeebe94bca3ab5d2f4d9a42f9d2e9c1c8
Rodrigo César <rodrigocnn@gmail.com>
2025-04-22 16:28:49 -0300

    chore: add and config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './main/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Título da sua API')
+    .setDescription('Descrição da sua API')
+    .setVersion('1.0')
+    .addBearerAuth() // se você quiser autenticação JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 8080);
